58234c5f39df68aaf4e8924af2686a19dc0cf466
Diego Cornejo Santibáñez <89801017+DiegoCornejoS@users.noreply.github.com>
2024-01-28 21:46:37 -0300

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,3 +1,18 @@
-if ( $a === $b ) { ?>
-<some html>
-<?php }
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
